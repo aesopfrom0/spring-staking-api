d93aecdd5bac1c2e9505bf2f86fda17a6f642aed
Aesop <ee.changsub@gmail.com>
2024-11-11 23:03:02 +0900

    Initial commit: Project setup with basic health check endpoint

---
+++ src/main/java/com/staking/staking_service/StakingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.staking.staking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StakingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StakingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/staking/staking_service/StakingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staking.staking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StakingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
